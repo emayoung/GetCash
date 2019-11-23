1fc5b505bb74d7796882fb671584acec0d932f7e
Emayoung <emayoung95@gmail.com>
2019-11-24 00:06:51 +0100

    Initial Commit

---
+++ GetCash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetCash
+//
+//  Created by Ememobong Akpanekpo on 24/11/2019.
+//  Copyright © 2019 Ememobong Akpanekpo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

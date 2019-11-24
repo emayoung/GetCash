415371fa49a19b9ec7f58ce6f6c75a1957e06aef
Emayoung <emayoung95@gmail.com>
2019-11-24 20:04:11 +0100

    Implement on boarding design

---
+++ GetCash/Controller/Authentication/OnboardingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OnboardingViewController.swift
+//  GetCash
+//
+//  Created by Ememobong Akpanekpo on 24/11/2019.
+//  Copyright © 2019 Ememobong Akpanekpo. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

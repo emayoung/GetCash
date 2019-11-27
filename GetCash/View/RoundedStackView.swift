2460528859ec2ec9780d2fc08ea7b63da97a4e40
Emayoung <emayoung95@gmail.com>
2019-11-27 15:11:25 +0100

    Add .gitignore file

---
+++ GetCash/View/RoundedStackView.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedStackView.swift
+//  GetCash
+//
+//  Created by Ememobong Akpanekpo on 27/11/2019.
+//  Copyright © 2019 Ememobong Akpanekpo. All rights reserved.
+//
+
+import UIKit
+
+class RoundedStackView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
